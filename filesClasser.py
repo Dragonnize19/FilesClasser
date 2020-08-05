4268be8421b382b61a38fec02fe3c0d5de24c2c7
Walid Mahfoud <53349530+Dragonnize19@users.noreply.github.com>
2020-08-05 09:24:27 +0100

    Add files via upload

---
+++ filesClasser.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import glob
+import shutil
+import tkinter as tk
+from tkinter import filedialog
+rep = 'Y'
+root = tk.Tk()
+root.withdraw()
+while rep == 'Y':
+    cwd = filedialog.askdirectory()
+    print('Current directory :', cwd)
+    nbrExtention = int(input('How many folder : '))
+    directories = []
+    extentions = []
+    for i in range(nbrExtention):
+        directorie = input(f'Name of folder {i + 1} : ')
+        extention = input('Name of extention : ')
+        directories.append(directorie)
+        extentions.append("/*." + extention)
+    for i in range(nbrExtention):
+        files = [f for f in glob.glob(cwd + extentions[i])] 
+        inPath = "/" + directories[i]
+        path = cwd + inPath            
+        try:
+            os.mkdir(path)
+        except OSError:
+            print ("Creation of the directory %s failed" % path)
+        else:
+            print ("Successfully created the directory %s " % path)
+        try:
+            for file in files : 
+                shutil.move(file, path)
+        except OSError:
+            print ("Error !")
+        else:
+            print("Success !")
+    
+    rep = input('Do it again ? (Y/N) : ')
